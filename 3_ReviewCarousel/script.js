8f093662f2eb99dc7583e0c6da196a1a0c3383d2
Shmaiter Morales <shmaither@gmail.com>
2023-10-25 15:58:56 -0600

    feature: surprise button returns a random review from a db

--- 3_ReviewCarousel/script.js
+++ 3_ReviewCarousel/script.js
@@ -1,3 +1,37 @@
 import { reviews } from "./db.js";
 
-// console.log(reviews[0]);
+let profilePhoto = document.querySelector("#person-img");
+let name = document.querySelector(".name");
+let job = document.querySelector(".job");
+let text = document.querySelector(".text");
+let surpriseBtn = document.querySelector(".surprise");
+
+function randomReview() {
+  let randomNum = Math.floor(Math.random() * 4);
+  return reviews[randomNum];
+}
+
+function modifyCardContent(review) {
+  profilePhoto.src = review.img;
+  name.textContent = firstLetterToUpperCase(review.name);
+  job.textContent = review.job.toUpperCase();
+  text.textContent = review.text;
+}
+
+function firstLetterToUpperCase(str) {
+  let wordsArray = str.split(" ");
+  let newString = wordsArray
+    .map((word) => {
+      return word.charAt(0).toUpperCase() + word.substring(1);
+    })
+    .join(" ");
+  return newString;
+}
+
+window.addEventListener("load", () => {
+  modifyCardContent(randomReview());
+});
+
+surpriseBtn.addEventListener("click", () => {
+  modifyCardContent(randomReview());
+});
