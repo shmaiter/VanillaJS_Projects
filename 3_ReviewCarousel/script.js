b06496fdf134fcdfff5860a3eb1b4c0fa0e75c29
Shmaiter MR <shmaither@gmail.com>
2023-11-06 10:34:44 -0600

    feature: modify cards content base on left and right arrows

--- 3_ReviewCarousel/script.js
+++ 3_ReviewCarousel/script.js
@@ -1,10 +1,40 @@
 import { reviews } from "./db.js";
 
+let currentReview = {};
+
+window.addEventListener("load", () => {
+  modifyCardContent(randomReview());
+});
+
 let profilePhoto = document.querySelector("#person-img");
 let name = document.querySelector(".name");
 let job = document.querySelector(".job");
 let text = document.querySelector(".text");
 let surpriseBtn = document.querySelector(".surprise");
+let leftBtn = document.querySelector(".signals").firstElementChild;
+let rightBtn = document.querySelector(".signals").lastElementChild;
+
+rightBtn.addEventListener("click", () => {
+  let reviewIndex = currentReview.id - 1;
+  if (reviewIndex == 3) {
+    console.log("New Index: " + 0);
+    modifyCardContent(reviews[0]);
+  } else {
+    console.log("New Index: " + (reviewIndex + 1));
+    modifyCardContent(reviews[reviewIndex + 1]);
+  }
+});
+
+leftBtn.addEventListener("click", () => {
+  let reviewIndex = currentReview.id - 1;
+  if (reviewIndex == 0) {
+    console.log("New Index: " + (reviews.length - 1));
+    modifyCardContent(reviews[reviews.length - 1]);
+  } else {
+    console.log("New Index: " + (reviewIndex - 1));
+    modifyCardContent(reviews[reviewIndex - 1]);
+  }
+});
 
 function randomReview() {
   let randomNum = Math.floor(Math.random() * 4);
@@ -12,6 +42,7 @@ function randomReview() {
 }
 
 function modifyCardContent(review) {
+  currentReview = review;
   profilePhoto.src = review.img;
   name.textContent = firstLetterToUpperCase(review.name);
   job.textContent = review.job.toUpperCase();
@@ -27,10 +58,6 @@ function firstLetterToUpperCase(str) {
     .join(" ");
   return newString;
 }
-
-window.addEventListener("load", () => {
-  modifyCardContent(randomReview());
-});
 
 surpriseBtn.addEventListener("click", () => {
   modifyCardContent(randomReview());
